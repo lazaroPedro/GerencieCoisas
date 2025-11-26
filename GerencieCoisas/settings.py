880b24e07a25a330462c8c52d07c4cc66bde63aa
Lázaro Pedro <lazaro.p806@gmail.com>
2025-11-25 21:13:36 -0300

    feat: create app categories

--- GerencieCoisas/settings.py
+++ GerencieCoisas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'produtos.apps.ProdutosConfig'
+    'produtos.apps.ProdutosConfig',
+    'categorias.apps.CategoriasConfig'
 ]
 
 MIDDLEWARE = [
